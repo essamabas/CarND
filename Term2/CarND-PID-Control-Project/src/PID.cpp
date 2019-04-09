c55c5ba62237bc1cda902bd95184286414534ab1
essam abas <essam.abas@outlook.com>
2019-04-09 05:54:29 +0200

    PID- Controller Parameters

--- Term2/CarND-PID-Control-Project/src/PID.cpp
+++ Term2/CarND-PID-Control-Project/src/PID.cpp
@@ -1,4 +1,6 @@
 #include "PID.h"
+#include <iostream>
+
 
 /**
  * TODO: Complete the PID class. You may add any additional desired functions.
@@ -44,7 +46,58 @@ double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return (this->p_error * this->Kp) + 
+  double steer = (this->p_error * this->Kp) + 
          (this->i_error * this->Ki) + 
          (this->d_error * this->Kd);  // TODO: Add your total error calc here!
+  if (steer < -1) {
+    steer = -1;
+  }
+  if (steer > 1) {
+    steer = 1;
+  }
+  return steer;
 }
+
+/*
+void PID::Twiddle(double total_error, double hyperparameter) {
+  static double current_best_error = 100000;
+  static bool is_twiddle_init = false;
+  static bool is_twiddle_reset = false;
+  static double last_hyperp = 0;
+  std::cout<<"Current best error is: "<< current_best_error<<endl;
+  std::cout<<"Dp is: "<<delta_p<<endl;
+  if (!is_twiddle_init) {
+  	std::cout<<"Twiddle init";
+  	current_best_error = total_error;
+  	is_twiddle_init = true;
+  	return;
+  }
+  if ((fabs(delta_p) > tolerance)) {
+  	if (is_twiddle_reset) {
+  		std::cout<<"Twiddle reset!-----------------------------"<<endl;
+  		last_hyperp = hyperparameter;
+  		hyperparameter += delta_p;
+  		std::cout<<"Hyperparameter magnitude increased!"<<endl;
+  		is_twiddle_reset = false;
+  	} else {
+  		if (total_error < current_best_error) {
+  			delta_p *= 1.1;
+  			is_twiddle_reset = true;
+  			current_best_error = total_error;
+  		} else {
+  			if (fabs(last_hyperp) < fabs(hyperparameter)) {
+  				last_hyperp = hyperparameter;
+  				hyperparameter -= 2.0 * delta_p;
+  				std::cout<<"Hyperparameter magnitude decreased!"<<endl;
+  			} else {
+  				last_hyperp = hyperparameter;
+  				hyperparameter += delta_p;
+  				delta_p *= 0.9;
+  				std::cout<<"Hyperparameter magnitude kept same!"<<endl;
+  				is_twiddle_reset = true;
+  			}
+  		}
+  	}
+  }
+}
+*/
--- Term2/CarND-PID-Control-Project/src/main.cpp
+++ Term2/CarND-PID-Control-Project/src/main.cpp
@@ -32,11 +32,12 @@ string hasData(string s) {
 
 int main() {
   uWS::Hub h;
-
+  
   PID pid;
   /**
    * TODO: Initialize the pid variable.
    */
+  pid.Init(.091 , .0005 , 1.693);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -63,6 +64,8 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
